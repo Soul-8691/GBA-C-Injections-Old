bfda334f426e4c0a40cbf5b56a0dd7880d9fe944
payst-t <payst.type@gmail.com>
2024-07-29 04:56:17 +0300

    Forgot the braces...

--- src/test.c
+++ src/test.c
@@ -1,6 +1,6 @@
 #include "../include/global.h"
 
-const u16 gSomePalette = INCBIN_U16("graphics/4bpp/abra_front.gbapal");
+const u16 gSomePalette[] = INCBIN_U16("graphics/4bpp/abra_front.gbapal");
 
 void TestFunc()
 {
