18871d6f2ba3ef1109180cbb0baba8e9c3580819
Petuuuhhh <peterswanton191@bellsouth.net>
2024-07-29 21:07:33 -0400

    IDK

--- src/test.c
+++ src/test.c
@@ -2,13 +2,13 @@
 #include "../include/decompress.h"
 #include "../include/palette.h"
 
-extern const u16 sCopyright_Pal[];// = INCBIN_U16("graphics/4bpp/intro/copyright.gbapal");
-extern const u8 sCopyright_Gfx[];//  = INCBIN_U8( "graphics/4bpp/intro/copyright.4bpp.lz");
-extern const u8 sCopyright_Map[];//  = INCBIN_U8( "graphics/4bpp/intro/copyright.bin.lz");
+static const u16 sCopyright_Pal_[];// = INCBIN_U16("graphics/4bpp/intro/copyright.gbapal");
+static const u8 sCopyright_Gfx_[];//  = INCBIN_U8( "graphics/4bpp/intro/copyright.4bpp.lz");
+static const u8 sCopyright_Map_[];//  = INCBIN_U8( "graphics/4bpp/intro/copyright.bin.lz");
 
 void LoadCopyrightGraphics(u16 charBase, u16 screenBase, u16 palOffset)
 {
-    LZ77UnCompVram(sCopyright_Gfx, (void *)BG_VRAM + charBase);
-    LZ77UnCompVram(sCopyright_Map, (void *)BG_VRAM + screenBase);
-    LoadPalette(sCopyright_Pal, palOffset, 0x20);
+    LZ77UnCompVram(sCopyright_Gfx_, (void *)BG_VRAM + charBase);
+    LZ77UnCompVram(sCopyright_Map_, (void *)BG_VRAM + screenBase);
+    LoadPalette(sCopyright_Pal_, palOffset, 0x20);
 }
