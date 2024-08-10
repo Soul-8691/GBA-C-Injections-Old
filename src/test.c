450ce43a9c879886cc54a34e9c66ba9d998dfa2e
Petuuuhhh <peterswanton191@bellsouth.net>
2024-08-10 09:50:37 -0400

    Update

--- src/test.c
+++ src/test.c
@@ -1,6 +1,6 @@
 #include "../include/gba/types.h"
 
-const u8 gTable[] = INCBIN_U8(
+const u8 gCardGraphics[] = INCBIN_U8(
 	"graphics/Resize/gCardGraphicsNone.6bpp",
 	"graphics/Resize/gCardGraphics7ColoredFish.6bpp",
 	"graphics/Resize/gCardGraphics7Completed.6bpp",
@@ -824,7 +824,7 @@ const u8 gTable[] = INCBIN_U8(
 	"graphics/Resize/gCardGraphicsInsectMonsterToken.6bpp"
 );
 
-const u8 gTablePals[] = INCBIN_U8(
+const u8 gCardPalettes[] = INCBIN_U8(
 	"graphics/Resize/gCardGraphicsNone.gbapal",
 	"graphics/Resize/gCardGraphics7ColoredFish.gbapal",
 	"graphics/Resize/gCardGraphics7Completed.gbapal",
